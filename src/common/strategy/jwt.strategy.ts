cb6954ffd978812a607561a98b84451e26c21704
rthcoder <tohirdavrsmart@gmail.com>
2025-06-26 17:44:51 +0500

    fx

--- src/common/strategy/jwt.strategy.ts
+++ src/common/strategy/jwt.strategy.ts
@@ -10,7 +10,7 @@ export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_AT_SECRET'),
+      secretOrKey: configService.get<string>('JWT_SECRET_KEY'),
     });
   }
 
